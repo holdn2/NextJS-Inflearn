eaf0e7891eb5d468fbc5ffc37e20712b4b8e5c24
holdn2 <yoocy01@gmail.com>
2025-10-01 23:35:04 +0900

    feat: SSG 까지 실습 완료. 폴백 옵션 적용

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,13 +1,32 @@
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import styles from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-book";
+import { useRouter } from "next/router";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
+export const getStaticPaths = () => {
+  return {
+    paths: [
+      { params: { id: "1" } },
+      { params: { id: "2" } },
+      { params: { id: "3" } },
+    ],
+    fallback: true,
+    // false : 404 Not found 페이지로 보냄
+    // blocking : SSR 방식으로 생성
+    // true : SSR 방식 + 데이터가 없는 폴백 상태의 페이지부터 반환
+  };
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
   const id = context.params!.id;
 
   const book = await fetchOneBook(Number(id));
+
+  if (!book) {
+    return {
+      notFound: true,
+    };
+  }
 
   return {
     props: { book },
@@ -16,8 +35,12 @@ export const getServerSideProps = async (
 
 export default function Page({
   book,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const router = useRouter();
+
+  if (router.isFallback) return "로딩 중입니다.";
   if (!book) return "문제가 발생했습니다. 다시 시도해주세요";
+
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,19 @@ import { ReactNode } from "react";
 import SearchableLayout from "@/components/searchable-layout";
 import styles from "./index.module.css";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType } from "next";
+import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   // 페이지 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터(백엔드 데이터 등)를 불러오는 함수
   // 사전 렌더링 과정에서 딱 한번만 실행됨
   // 서버측에서만 실행되는 함수
   // 서버 환경에서만 실행되기 때문에 브라우저를 읽을 수 없음
 
   // console.log("asdfasdf"); => 이런 것은 브라우저 콘솔에 출력되지 않음. 터미널에서는 출력됨
+
+  console.log("인덱스 페이지");
 
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
@@ -31,8 +33,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(allBooks);
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={styles.container}>
       <section>
